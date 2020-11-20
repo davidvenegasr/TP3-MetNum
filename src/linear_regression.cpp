ed56e683d7f37316fb6d721f4d3ed6ad132210f6
David Venegas <davidvenegasr@gmail.com>
2020-11-20 17:01:05 -0300

    first steps

--- src/linear_regression.cpp
+++ src/linear_regression.cpp
@@ -10,16 +10,25 @@ namespace py=pybind11;
 
 LinearRegression::LinearRegression()
 {
+    // Inicializamos la estructura de regresor lineal para los coeficientes en el vector W
+    Matrix m;
+    this-> W = m;
 }
 
 void LinearRegression::fit(Matrix X, Matrix y)
 {
+
+    Matrix XT = X.transpose();
+    
+    //W^ =(XTX)^−1 XTy
+    W = (XT*X).inverse()* XT*y;
+
 }
 
 
 Matrix LinearRegression::predict(Matrix X)
 {
     auto ret = MatrixXd::Zero(X.rows(), 1);
-
+    Matrix y = X*W;
     return ret;
 }
